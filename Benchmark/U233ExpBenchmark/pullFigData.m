9429870da5d708b978c1aa8f5d48be14e54079f2
Visura Pathirana <49644763+visura96@users.noreply.github.com>
2023-04-21 17:49:29 -0400

    Delete Benchmark directory

--- Benchmark/U233ExpBenchmark/pullFigData.m
+++
@@ -1,9 +0,0 @@
-fig = gcf;
-axObjs = fig.Children;
-dataObjs = findobj(fig,'-property','YData');
-dataObjs = findobj(fig,'-property','XData');
-y = dataObjs(1).YData;
-x = dataObjs(1).XData;
-
-save('U233EXP8MW.mat','x','y')
-
